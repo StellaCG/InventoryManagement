6a67facd143633445cf68ab808c7ae5c637d20a4
StellaCG <tristanlhale@gmail.com>
2022-07-27 12:21:20 -0400

    added json converters

---
+++ Library.InventoryManagement/Utility/ItemJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.InventoryManagement.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Library.InventoryManagement.Utility
+{
+    public class ItemJsonConverter : JsonCreationConverter<Item>
+    {
+        protected override Item Create(Type objectType, JObject jObject)
+        {
+            if (jObject == null) throw new ArgumentException("jObject");
+
+            if (jObject["weight"] != null || jObject["Weight"] != null)
+            {
+                return new InventoryManagement.Models.ProductByWeight();
+            } else if (jObject["quantity"] != null || jObject["Quantity"] != null)
+            {
+                return new Product();
+            } else
+            {
+                return new Item();
+            }
+        }
+    }
+}
